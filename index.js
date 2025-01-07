ca4ed60017425ee13fd0de87dd111207fd836b75
Frank <franco.arteche.2b@gmail.com>
2025-01-07 16:58:00 -0300

    All through 3.6 done

--- index.js
+++ index.js
@@ -1,88 +1,91 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <br/>
     <p>${new Date()}</p>
-    `)
-})
+    `);
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+  const newID = Math.floor(Math.random() * 1000);
+  return String(newID);
+};
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(request);
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'Something failed' 
-    })
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Something failed",
+    });
+  } else if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
   }
 
   const person = {
     id: generateId(),
     name: body.name,
-    number: body.number
-  }
+    number: body.number,
+  };
 
-  persons = persons.concat(person)
+  persons = persons.concat(person);
 
-  response.json(person)
-})
+  response.json(person);
+});
