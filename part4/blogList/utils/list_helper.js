93bb4e3440ba03090c6283d9519d1b682188aad2
Frank <franco.arteche.2b@gmail.com>
2025-01-14 19:51:48 -0300

    Exercise 4.4
    First unit tests created and working

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes  = (blogs) => {
+
+    let totalLikes = 0
+
+    blogs.forEach(element => {
+        totalLikes =+ element.likes
+    });
+
+    return totalLikes
+}
+
 module.exports = {
   dummy,
+  totalLikes 
 };
