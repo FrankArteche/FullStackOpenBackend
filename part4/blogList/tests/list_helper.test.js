f7fa8a13df24653da604f29bda6cc8b3d096752e
Frank <franco.arteche.2b@gmail.com>
2025-01-16 10:34:30 -0300

    Exercise 4.5 done!

--- part4/blogList/tests/list_helper.test.js
+++ part4/blogList/tests/list_helper.test.js
@@ -1,18 +1,18 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
 
-describe('total likes', () => {
+describe("total likes", () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 5,
-      __v: 0
-    }
-  ]
+      __v: 0,
+    },
+  ];
 
   const blogs = [
     {
@@ -21,7 +21,7 @@ describe('total likes', () => {
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 7,
-      __v: 0
+      __v: 0,
     },
     {
       _id: "5a422aa71b54a676234d17f8",
@@ -29,7 +29,7 @@ describe('total likes', () => {
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
+      __v: 0,
     },
     {
       _id: "5a422b3a1b54a676234d17f9",
@@ -37,7 +37,7 @@ describe('total likes', () => {
       author: "Edsger W. Dijkstra",
       url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
       likes: 12,
-      __v: 0
+      __v: 0,
     },
     {
       _id: "5a422b891b54a676234d17fa",
@@ -45,7 +45,7 @@ describe('total likes', () => {
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
       likes: 10,
-      __v: 0
+      __v: 0,
     },
     {
       _id: "5a422ba71b54a676234d17fb",
@@ -53,7 +53,7 @@ describe('total likes', () => {
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
       likes: 0,
-      __v: 0
+      __v: 0,
     },
     {
       _id: "5a422bc61b54a676234d17fc",
@@ -61,23 +61,91 @@ describe('total likes', () => {
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
       likes: 2,
-      __v: 0
-    }  
-  ]
+      __v: 0,
+    },
+  ];
 
-  test('of empty list is zero', () => {
-    const blogs = []
-  
-    const result = listHelper.dummy(blogs)
-    assert.strictEqual(result, 1)
-  })
+  test("of empty list is zero", () => {
+    const blogs = [];
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
-  })
+    const result = listHelper.dummy(blogs);
+    assert.strictEqual(result, 1);
+  });
 
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(blogs)
-  })
-})
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(blogs);
+
+    assert.strictEqual(result, 36);
+  });
+});
+
+describe("Favorite blog", () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0,
+    },
+  ];
+
+  test("of a list", () => {
+    const result = listHelper.favoriteBlog(blogs);
+
+    assert.deepStrictEqual(result, {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    });
+  });
+});
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -7,13 +7,29 @@ const totalLikes  = (blogs) => {
     let totalLikes = 0
 
     blogs.forEach(element => {
-        totalLikes =+ element.likes
+        totalLikes += element.likes
     });
 
     return totalLikes
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  let blogWithMostLikes = blogs[0]  // Inicializa con el primer blog
+
+  blogs.forEach((item) => {
+    if (item.likes > blogWithMostLikes.likes) {
+      blogWithMostLikes = item
+    }
+  })
+
+  return blogWithMostLikes
+}
+
+
 module.exports = {
   dummy,
-  totalLikes 
+  totalLikes,
+  favoriteBlog
 };
