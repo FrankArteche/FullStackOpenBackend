bfc08a7de6fddea3df811cb7210505a890a81494
Frank <franco.arteche.2b@gmail.com>
2025-01-22 00:10:06 -0300

    Exercise 4.8 done

---
+++ part4/blogList/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+
+const helper = require("../utils/list_helper");
+const app = require("../app");
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let blogObject = new Blog(helper.initialBlogs[0]);
+  await blogObject.save();
+
+  blogObject = new Blog(helper.initialBlogs[1]);
+  await blogObject.save();
+});
+
+test.only("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+after(async () => {
+  await mongoose.connection.close()
+})
