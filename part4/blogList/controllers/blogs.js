d76dd9c2900c0c538ec54a4609c027a3dd2c144c
Frank <franco.arteche.2b@gmail.com>
2025-01-26 20:46:18 -0300

    Exercise 4.17 done

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,44 +1,60 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const jwt = require('jsonwebtoken')
+const User = require("../models/user");
 
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
   }
-  return null
-}
+  return null;
+};
 
 blogsRouter.post("/", async (request, response) => {
-  const body = request.body
+  const body = request.body;
 
   if (!body.title || !body.url) {
     return response.status(400).json({ error: "Title and URL are required" });
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+  // const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({ error: 'token invalid' })
+  // }
+  // const existingUser = await User.findById(decodedToken.id)
+  const existingUser = await User.find({});
+  if (existingUser.length === 0) {
+    return response
+      .status(400)
+      .json({ error: "No users available in the database" });
   }
-  const existingUser = await User.findById(decodedToken.id)
+
+  let randomIndex = Math.floor(Math.random() * existingUser.length);
 
   const blog = new Blog({
     title: body.title,
-    authro: body.author,
+    author: body.author,
     url: body.url,
-    likes: body.likes,
-    user: existingUser._id
+    likes: body.likes || 0,
+    user: existingUser[randomIndex]._id,
   });
 
   const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(savedBlog._id)
+
+  if (!existingUser[randomIndex].blogs) {
+    existingUser[randomIndex].blogs = []; // Ensure blogs array exists
+  }
+  existingUser[randomIndex].blogs = existingUser[randomIndex].blogs.concat(savedBlog._id);
+
+  await existingUser[randomIndex].save();
+
   response.status(201).json(savedBlog);
 });
 
